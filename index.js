02c6437d486667eda711522489a6f5a223714dfa
dragon-qiu <253015291@qq.com>
2018-11-30 09:36:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Successful:Hello World!");
+    response.end("See pipeline success!");
 
 });
 
